6909fbab5524fd8c6bb0609837c349af2e998c06
JamesYin <3401754684@qq.com>
2019-06-20 12:30:11 +0800

    add detailPage

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -19,11 +19,11 @@
             </li> -->
 
             <li v-for="item in comingList" :key = "item.id">
-                <div class="pic_show">
+                <div class="pic_show" @tap="handleToDetail(item.id)">
                     <img :src="item.img | setWH('128.180')">
                 </div>
                 <div class="info_list">
-                    <h2>{{ item.nm }} <img v-if="item.version" src="@/assets/maxs.png"></h2></h2>
+                    <h2 @tap="handleToDetail(item.id)">{{ item.nm }} <img v-if="item.version" src="@/assets/maxs.png"></h2></h2>
                     <p>
                         <span class="person">{{ item.wish }}</span> 人想看
                     </p>
@@ -65,6 +65,11 @@ export default {
                 this.prevCityId = cityId;
             }
         })
+    },
+    methods: {
+        handleToDetail(movieId) {
+            this.$router.push('/movie/detail/2/' + movieId)
+        }
     }
 }
 </script>
--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -21,11 +21,11 @@
             <li class="pullDown">{{ pullDownMsg }}</li>
 
             <li v-for="item in movieList" :key="item.id">
-                <div class="pic_show" @tap="handleToDetail">
+                <div class="pic_show" @tap="handleToDetail(item.id)">
                     <img :src="item.img | setWH('128.180')">
                 </div>
                 <div class="info_list">
-                    <h2>{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png"></h2>
+                    <h2 @tap="handleToDetail(item.id)">{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png"></h2>
                     <p>观众评
                         <span class="grade">{{ item.sc }}</span>
                     </p>
@@ -108,8 +108,9 @@ export default {
         })
     },
     methods: {
-        handleToDetail() {
+        handleToDetail(movieId) {
             // console.log(222);
+            this.$router.push('/movie/detail/1/' + movieId);
         },
         handleToScroll(pos) {
             if( pos.y > 30) {
